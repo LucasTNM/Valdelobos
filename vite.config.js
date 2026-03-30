4b8e27b9d2e9d26ba1eda71ee651ad739a6b7a4d
Tony <lucasstma01@gmail.com>
2026-03-30 18:37:37 -0300

    Corrigindo base do Vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // SUBSTITUA pelo nome exato do seu repositório no GitHub, com as barras
-  base: '/https://github.com/LucasTNM/Valdelobos/', 
+  base: '/Valdelobos/', 
   build: {
     // Garante que os assets grandes não quebrem o build
     chunkSizeWarningLimit: 1500,
