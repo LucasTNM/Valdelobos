21d8cdf250d1c856ac6347f117062e057fb65056
Lucas Tony <148718831+LucasTNM@users.noreply.github.com>
2026-05-05 10:08:13 -0300

    Revert "Revert "Tony""

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import Level1_Arrival from './scenes/Level1_Arrival';
 import Level4_Camp from './scenes/Level4_Camp';
 import Level5_DarkForest from './scenes/Level5_DarkForest';
 import Level6_Road from './scenes/Level6_Road';
+import Level6_Escape from './scenes/level6_Escape';
 import GameOver from './scenes/GameOver';
 import IntroScene from './scenes/IntroScene';
 
@@ -37,12 +38,14 @@ const config = {
         Level4_Camp,
         Level5_DarkForest,
         Level6_Road,
+        Level6_Escape,
         GameOver
     ]
 };
 
 // Inicializa o jogo
 const game = new Phaser.Game(config);
+game.debugMode = false;
 
 // Redimensiona o jogo quando a janela mudar de tamanho
 window.addEventListener('resize', () => {
